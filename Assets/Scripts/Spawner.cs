6da502cd113f5ef8e9c89e8966cd4f111a360d46
Dvoryanoff <dvoryanoff@mail.ru>
2022-06-13 20:48:36 +0300

    Spawner Script and GameObject added

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,7 +1,36 @@
+
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    private Collider spawnArea;
+
+    public GameObject[] fruitPrefabs;
+
+    public float minSpawnDelay = 0.25f;
+    public float maxSpawnDelay = 1f;
+
+    public float minAngle = -15f;
+    public float maxAngle = 15f;
+
+    public float minForce = 18f;
+    public float maxForce = 22f;
+
+    public float maxLifetime = 5f;
+
+    private void Awake()
+    {
+        spawnArea = GetComponent<Collider>();
+    }
+
+    // private void OnEnable()
+    // {
+    //     StartCoroutine(Spawn());
+    // }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
 }

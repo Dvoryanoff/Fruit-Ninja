22d6288f8930f80acaf599335875cfa0d3649b5c
Dvoryanoff <dvoryanoff@mail.ru>
2022-06-15 16:43:20 +0300

    Add fading when lose and Newgame

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,12 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public TMP_Text scoreText;
+    public Image fadeImage;
     private int score;
     private Blade blade;
     private Spawner spawner;
@@ -21,8 +24,26 @@ public class GameManager : MonoBehaviour
 
     private void NewGame()
     {
+        Time.timeScale = 1f;
+        spawner.enabled = true;
+        blade.enabled = true;
         score = 0;
         scoreText.text = score.ToString();
+        Time.timeScale = 1f;
+        ClearScene();
+    }
+
+    private void ClearScene()
+    {
+        Fruit[] fruits = FindObjectsOfType<Fruit>();
+        foreach (Fruit fruit in fruits) {
+            Destroy(fruit.gameObject);
+        }
+
+        Bomb[] bombs = FindObjectsOfType<Bomb>();
+        foreach (Bomb bomb in bombs) {
+            Destroy(bomb.gameObject);
+        }
     }
 
     public void IncreaseScore(int amount)
@@ -35,5 +56,30 @@ public class GameManager : MonoBehaviour
     {
         spawner.enabled = false;
         blade.enabled = false;
+        StartCoroutine(ExplodeSequence());
+    }
+
+    private IEnumerator ExplodeSequence()
+    {
+        float elapsed = 0f;
+        float duration = 0.5f;
+        while (elapsed < duration) {
+            float t = Mathf.Clamp01(elapsed / duration);
+            fadeImage.color = Color.Lerp(Color.clear, Color.white, t);
+            Time.timeScale = 1f - t;
+            elapsed += Time.unscaledDeltaTime;
+            yield return null;
+        }
+
+        yield return new WaitForSecondsRealtime(1f);
+        NewGame();
+        elapsed = 0f;
+
+        while (elapsed < duration) {
+            float t = Mathf.Clamp01(elapsed / duration);
+            fadeImage.color = Color.Lerp(Color.white, Color.clear, t);
+            elapsed += Time.unscaledDeltaTime;
+            yield return null;
+        }
     }
 }
